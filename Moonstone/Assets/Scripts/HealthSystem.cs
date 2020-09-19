b3a737307c6d915e1063d6fb60a8a6f602010341
Galthukan <marc.r.n.andersson@gmail.com>
2020-09-19 21:32:12 +0200

    WIP Prototyping & Learning

---
+++ Moonstone/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+public class HealthSystem
+{
+    public int health { get; set; }
+
+    public int healthMax = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setHealth(100);
+        // Debug.Log("Health: " + getHealth());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health < 0)
+        {
+            // Debug.Log("Blue Knight is dead!");
+        }
+    }
+
+    public void setHealth(int health)
+    {
+        this.health = health;
+    }
+
+    public int getHealth()
+    {
+        return this.health;
+    }
+
+    // public event Action OnHealthZero;
+
+    public void Damage(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health > 0) { return; }
+
+        if (health < 0)
+        {
+            health = 0;
+            // Debug.Log("Player Health: " + health);
+            // OnHealthZero?.Invoke();
+        }
+    }
+
+    public void Heal(int healAmount)
+    {
+        health += healAmount;
+        if (health > healthMax)
+        {
+            health = healthMax;
+        }
+    }
+}
--- Moonstone/Assets/Scripts/PlayerCollision.cs
+++ Moonstone/Assets/Scripts/PlayerCollision.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public GameHandler gameHandler;
+    // public HealthSystem playerOneHP;
+
+    private void Awake() {
+        // gameHandler = GetComponent<GameHandler>();
+        // Debug.Log("healthMax" + gameHandler.healthSystem.healthMax);
+        // gameHandler = GetComponent<GameHandler>();
+
+    }
+
+    private void Start() {
+        // Debug.Log("The player health is: " + gameHandler.healthSystem.getHealth());
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log(other.collider.name);
+        if (other.collider.tag == "Obstacle")
+        {
+            // gameHandler.healthSystem.Damage(1);
+            GameHandler gameHandler = GetComponent<GameHandler>();
+            gameHandler.PlayerDamage(1);
+            // playerOneHP.Damage(1);
+            Debug.Log("1 Damage taken");
+        }
     }
 }
