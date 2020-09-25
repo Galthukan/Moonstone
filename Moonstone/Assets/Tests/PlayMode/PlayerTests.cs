66f01067dafcae657b3c4b9e459659bf9c996e84
Galthukan <marc.r.n.andersson@gmail.com>
2020-09-25 21:46:10 +0200

    Add Test Player lower health limit

--- Moonstone/Assets/Tests/PlayMode/PlayerTests.cs
+++ Moonstone/Assets/Tests/PlayMode/PlayerTests.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 namespace Tests.PlayMode
 {
@@ -16,6 +15,18 @@ namespace Tests.PlayMode
             myComponent.PlayerHealth.Damage(1);
 
             Assert.AreEqual(99, myComponent.PlayerHealth.Health);
+        }
+        
+        [Test]
+        public void TestPlayerHealthBelowZero()
+        {
+            var testObj = new GameObject();
+            var myComponent = testObj.AddComponent(typeof(PlayerHandler)) as PlayerHandler;
+            
+            myComponent.PlayerHealth.Damage(101);
+
+            Assert.AreEqual(0, myComponent.PlayerHealth.Health);
+            TestContext.WriteLine("Health: " + myComponent.PlayerHealth.Health);
         }
     }
 }
