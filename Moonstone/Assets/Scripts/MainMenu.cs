18da12465b12e65a75f698118222dca6cbe1a60f
Galthukan <marc.r.n.andersson@gmail.com>
2020-09-27 15:18:19 +0200

    Add MainMenu, Load Menu and OptionsMenu

---
+++ Moonstone/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        Button playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        playButton.Select();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
